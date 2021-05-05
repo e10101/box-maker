23b7607b505675e64ac7f136a3b9dcc60d49ff2f
Yishi Guo <baicaibang@gmail.com>
2021-05-05 22:10:55 +0800

    Add ability to generate random category names

--- src/box_maker/box_maker.py
+++ src/box_maker/box_maker.py
@@ -1,27 +1,53 @@
 import pascal_voc_writer
+import os
+import random
+import logging
+import sys
+from coolname import generate_slug
+
+
+def _re_seed(seed=0):
+    random.seed(os.urandom(seed))
+
+
+def _generate_category_names(num):
+    num_of_leading_zero = len(str(num))
+    return ['{}-{}'.format(generate_slug(2), str(i+1).zfill(num_of_leading_zero)) for i in range(num)]
 
 
 class BoxMaker:
-	def __init__(self, count: int = 10):
-		self._count = count
+    def __init__(self,
+                 num_of_categories,
+                 verbose=False,
+                 seed: int = 0,
+                 count: int = 10,
+                 **kwargs):
+        _re_seed(seed)
+        self._count = count
+        self._num_of_categories = num_of_categories
+        self._category_names = _generate_category_names(num_of_categories)
 
-	def test(self):
-		writer = pascal_voc_writer.Writer(
-			path='hello.jpg',
-			width=600,
-			height=400,
-		)
+        if verbose:
+            logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
-		writer.addObject(
-			name='category1',
-			xmin=10,
-			ymin=20,
-			xmax=30,
-			ymax=40,
-		)
+        logging.debug('self._category_names: {}'.format(str(self._category_names)))
 
-		writer.save('this.xml')
+    def test(self):
+        writer = pascal_voc_writer.Writer(
+            path='hello.jpg',
+            width=600,
+            height=400,
+        )
 
-	def hello(self):
-		print('Hello world!')
+        writer.addObject(
+            name='category1',
+            xmin=10,
+            ymin=20,
+            xmax=30,
+            ymax=40,
+        )
 
+        writer.save('this.xml')
+
+    def hello(self):
+        print('Hello world!')
--- src/box_maker/scripts/cli.py
+++ src/box_maker/scripts/cli.py
@@ -1,11 +1,33 @@
 import click
 import box_maker
 
+
 @click.command()
-def entry():
+@click.option('--width', '-w', default=640, type=int, show_default=True,
+              help='The width of generated image.')
+@click.option('--height', '-h', default=640, type=int, show_default=True,
+              help='The height of generated image.')
+@click.option('--num-of-images', '-n', default=100, type=int, show_default=True,
+              help='The number of images will be generated.')
+@click.option('--num-of-categories', '-c', default=10, type=int,
+              help='The number of categories.')
+@click.option('--num-of-instances-per-category', default='[100]', show_default=True,
+              help='Repeated number for each category. e.g. if num-of-categories is 8, '
+                   'and this option is [1, 3 ,2], then the number of '
+                   'instance for each category will be [1, 3, 2, 1, 3, 2, 1, 3]')
+@click.option('--type', '-t', default='PASCAL', show_default=True,
+              type=click.Choice(['PASCAL'], case_sensitive=False))
+@click.option('--seed', default=0, show_default=True, type=int, help='Random seed.')
+@click.option('--output-path', '-o', default='./', type=str, show_default=True)
+@click.option('--verbose', '-v', is_flag=True, help='Increase output verbosity')
+def entry(**kwargs):
     """box-maker command line."""
-    bm = box_maker.BoxMaker()
+    bm = box_maker.BoxMaker(
+        **kwargs,
+    )
     bm.hello()
+    print('kwargs', kwargs, kwargs['height'])
+
 
 @click.command()
 def test():
